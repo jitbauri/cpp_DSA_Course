7f6f484ede1e8f1f444c021f3ec70daa9812ef90
Jit Bauri <jitbauri989@gmail.com>
2025-04-22 00:24:21 +0530

    Add 2D Dynamic array Codes

---
+++ codes/Dynamic Array/2D_Dynamic_Arr.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+//create 2D Dynamic Array
+int main(){
+    int rows,cols;
+    cout<<"enter rows :";
+    cin>>rows;
+    cout<<"enetr cols :";
+    cin>>cols;
+
+    int **matrix=new int*[rows];
+    for(int i=0;i<rows;i++){
+        matrix[i]=new int[cols];
+    }
+
+    //data store
+    int x=1;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            matrix[i][j]=x++;
+            cout<<matrix[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+return 0;
+}
